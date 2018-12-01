2d9f69894a8266d7111eabb1c02e0a7e53029cd5
Филиппов Кирилл Андреевич <filippov.kirill.andreevich@mail.ru>
2018-12-01 11:53:47 +0300

    Добавлен запрос в файл "task1.sql" к БД на выбор сотрудников, у которых день рождения на момент выполнения запроса

--- task1.sql
+++ task1.sql
@@ -1,2 +1,8 @@
--- ╨б╨╛╤В╤А╤Г╨┤╨╜╨╕╨║╨╕ ╨┐╨╛ ╨░╨╗╤Д╨╛╨▓╨╕╤В╤Г, ╨║╨╛╤В╨╛╤А╤Л╨╡ ╨╢╨╕╨▓╤Г╤В ╨▓ ╨│╨╛╤А╨╛╨┤╨░╤Е ╨╜╨░ "╨Р"
-SELECT name, city FROM employee WHERE city LIKE '╨Р%' ORDER BY name;
+SET NAMES 'cp866';
+
+use internship
+-- 1. Сотрудники по алфовиту, которые живут в городах на "А"
+SELECT name, city FROM employee WHERE city LIKE 'А%' ORDER BY name;
+
+-- 2. Сотрудники у которых сегодня день рождения
+SELECT name, birthday FROM employee WHERE birthday LIKE CONCAT('%', DATE_format(CURDATE(), '%m-%d'));
