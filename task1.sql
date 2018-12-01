9fcabca11a406c0250b8e339b06af7ea5cc3e343
Филиппов Кирилл Андреевич <filippov.kirill.andreevich@mail.ru>
2018-12-01 11:56:37 +0300

    Добавлен запрос в файл "task1.sql" к БД на выбор сотрудников, у которых максимальная и минимальная з/п

--- task1.sql
+++ task1.sql
@@ -6,3 +6,6 @@ SELECT name, city FROM employee WHERE city LIKE 'А%' ORDER BY name;
 
 -- 2. Сотрудники у которых сегодня день рождения
 SELECT name, birthday FROM employee WHERE birthday LIKE CONCAT('%', DATE_format(CURDATE(), '%m-%d'));
+
+-- 3. Сотрудники c минимальной и максимальной з/п
+SELECT name, salary FROM employee WHERE salary=(SELECT max(salary) FROM employee) OR salary=(SELECT min(salary) FROM employee)
